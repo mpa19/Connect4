0922ab6219c10b40b69297bf055fcbaef907a27e
mpa19 <marcperezarnaiz@gmail.com>
2019-04-24 20:00:13 +0200

    Primers pasoss

---
+++ app/src/main/java/com/example/marc/connect4/Inicio.java
@@ -0,0 +1,20 @@
+package com.example.marc.connect4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+
+    void gotoAjuda(View v){
+        Intent a = new Intent(this, Ajuda.class);
+        startActivity(a);
+    }
+}
---
+++ app/src/main/java/com/example/marc/connect4/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.marc.connect4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(3*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),Inicio.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
+    }
+
+
+}
