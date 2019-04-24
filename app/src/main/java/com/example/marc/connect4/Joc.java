4528ab6a47c67061c74070e8c8ec06d009045e13
mpa19 <marcperezarnaiz@gmail.com>
2019-04-24 21:43:17 +0200

    Correció colors i fons

---
+++ app/src/main/java/com/example/marc/connect4/Joc.java
@@ -0,0 +1,13 @@
+package com.example.marc.connect4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Joc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joc);
+    }
+}
